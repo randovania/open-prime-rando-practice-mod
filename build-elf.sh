56bb8e15c5ed0dac89992b47e152d244201b280c
duncathan <dunc@duncathan.com>
2026-05-13 12:43:48 -0600

    add pickup map icons

--- build-elf.sh
+++ build-elf.sh
@@ -6,11 +6,16 @@ OUTPUT_ELF=$1
 
 test -n "${OUTPUT_ELF}" || (echo "Output ELF path not provided" && exit 1)
 
+# param 2: build type (Debug/Release) - default Debug
+BUILD_TYPE="${2:-Release}"
+# make build dir names lowercase (this is more cross-platform)
+BUILD_TYPE_LOWER="$(echo "$BUILD_TYPE" | tr '[:upper:]' '[:lower:]')"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "${DIR}"
 
 IMAGE="ghcr.io/metroidprimemodding/gcn-static-patcher/build:latest"
-CMAKE_DIR="cmake-build-release-docker"
+CMAKE_DIR="cmake-build-${BUILD_TYPE_LOWER}-docker"
 EXTERNAL_SRC_DIR="$(pwd)/"
 EXTERNAL_BUILD_DIR="${EXTERNAL_SRC_DIR}${CMAKE_DIR}"
 DOCKER_SRC_DIR="/tmp/prime2-practice-mod/"
@@ -25,7 +30,7 @@ docker run --rm \
     -w "${DOCKER_BUILD_DIR}" \
     -v "${EXTERNAL_SRC_DIR}":"${DOCKER_SRC_DIR}" \
     "${IMAGE}" \
-    bash -c "cmake '${DOCKER_SRC_DIR}/external/prime2-practice-mod' -DCMAKE_BUILD_TYPE=Release -G Ninja && cmake --build . --config Release"
+    bash -c "cmake '${DOCKER_SRC_DIR}/external/prime2-practice-mod' -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -DRANDOMIZER=ON -G Ninja && cmake --build . --config ${BUILD_TYPE}"
 
 # Copy the elf
 cp "${EXTERNAL_BUILD_DIR}/prime2-practice" "${OUTPUT_ELF}"
